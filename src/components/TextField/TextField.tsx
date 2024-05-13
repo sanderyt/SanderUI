0c410dd199d84e3c554609fce169332b75bcc7ec
Sander van Rijsoort <s.rijsoort@gmail.com>
2024-05-13 20:13:12 +0100

    forwardRef for TextField component

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,35 +1,30 @@
-import React, { type FC } from "react";
 import classNames from "classnames/bind";
-
-import styles from "./TextField.module.scss";
+import React, { forwardRef } from "react";
 
 import type { TextFieldProps } from "./TextField.types";
 
+import styles from "./TextField.module.scss";
+
 const cx = classNames.bind(styles);
 
-const TextField: FC<TextFieldProps> = ({
-  name,
-  label,
-  type = "text",
-  error,
-  onChange,
-  ...props
-}) => {
-  return (
-    <div className={styles.container}>
-      <label className={styles.label}>
-        {label}
-        <input
-          type={type}
-          className={cx("textField", { textFieldError: error })}
-          onChange={onChange}
-          name={name}
-          {...props}
-        />
-      </label>
-      <div className={styles.error}>{error?.message}</div>
-    </div>
-  );
-};
+const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
+  ({ name, label, type = "text", error, ...props }, ref) => {
+    return (
+      <div className={styles.container}>
+        <label className={styles.label}>
+          {label}
+          <input
+            name={name}
+            type={type}
+            className={cx("textField", { textFieldError: error })}
+            ref={ref}
+            {...props}
+          />
+        </label>
+        {error && <div className={styles.error}>{error.message}</div>}
+      </div>
+    );
+  }
+);
 
 export default TextField;
